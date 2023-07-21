2fe99671f5d727227ad7003e8dd3afc2720620ed
luix gabriel <luisgasbriel@gmail.com>
2023-07-21 19:02:26 -0300

    chore: add config eslint prettier prisma and express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import express from 'express'
+import routes from './routes/route'
+
+const app = express()
+const port = 3000
+
+app.use('/', routes)
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
