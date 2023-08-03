18c5550a3c2facd07fa69aba51913eb43303b3e8
Bruno Freire <brunofreire1717@gmail.com>
2023-08-03 01:26:30 -0500

    feat: Add email validation and transformation middleware
    Created a new 'middlewares' directory to house reusable middleware functions. Added a new middleware named 'validateAndTransformEmail' for validating and transforming email. This middleware will be used for email-related operations in the application.
    
    Also, added 'dotenv' and 'nodemailer' packages for environment variables management and email sending, respectively. The server now opens on the port specified in the '.env' file under the 'PORT' variable, and if not provided, it opens on port 3000 by default.

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from 'express'
 import 'module-alias/register'
 import userRoutes from '../src/routes/user-routes'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/', userRoutes)
