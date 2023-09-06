3b822439ea050f94144c87f498211534769c8088
luix gabriel <luisgasbriel@gmail.com>
2023-09-06 19:27:29 -0300

    feat: add pagination in prisma-get-all-posts file

--- src/repositories/postRepositories/getPosts/prisma-get-all-posts.ts
+++ src/repositories/postRepositories/getPosts/prisma-get-all-posts.ts
@@ -7,7 +7,13 @@ import { Post } from '@prisma/client'
 
 export class PrismaGetAllPostsRepository implements IGetAllPostsRepository {
   async getAllPosts(navigation: IGetAllPostParams): Promise<Post[]> {
+    console.log(navigation)
     const posts = await prisma.post.findMany({
+      skip: navigation.offset,
+      take: navigation.limit,
+      orderBy: {
+        createdAt: 'desc',
+      },
       include: {
         author: true,
       },
