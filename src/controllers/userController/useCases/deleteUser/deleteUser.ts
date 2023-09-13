d576b1dca4c40ca7ca165cac74d45b7e849edf0f
Bruno Freire <brunofreire1717@gmail.com>
2023-09-12 23:01:58 -0500

    conflict-free update

--- src/controllers/userController/useCases/deleteUser/deleteUser.ts
+++ src/controllers/userController/useCases/deleteUser/deleteUser.ts
@@ -1,17 +1,15 @@
-import { deleteUserPrisma } from '@/repositories/userRepositories/deleteUser/prisma-delete-user'
-import { Request, Response } from 'express'
+import { deleteUserPrisma } from "@/repositories/userRepositories/deleteUser/prisma-delete-user";
+import { Request, Response } from "express"
 
 export const deleteUser = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(400).json({ error: 'Required user' })
+  if(!req.user){
+    return res.status(400).json({error: "Required user"})
   }
 
   try {
-    const userDeleted = await deleteUserPrisma(req.user)
+    const userDeleted = await deleteUserPrisma(req.user);
     res.status(200).json(`User deleted: ${userDeleted}`)
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: 'An error occurred while deleting the user.' })
+    res.status(500).json({ error: "An error occurred while deleting the user." });
   }
 }
