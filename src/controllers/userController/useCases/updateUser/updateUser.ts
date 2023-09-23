1472fac3665374a721d9830dc5804b9d6f7a1356
Bruno Freire <brunofreire1717@gmail.com>
2023-09-23 00:35:55 -0500

    feature: It is only possible to change the password using passwordrequest

--- src/controllers/userController/useCases/updateUser/updateUser.ts
+++ src/controllers/userController/useCases/updateUser/updateUser.ts
@@ -1,13 +1,10 @@
-import { passwordValidator } from "@/middlewares/validators/passwordValidator";
 import { updateUserPrisma } from "@/repositories/userRepositories/updateUser/prisma-update-user";
 import { Request, Response } from "express"
-import bcrypt from 'bcrypt';
 import { validateStack } from "@/utils/userUtils/validateStack";
 import { getUserPrisma } from "@/repositories/userRepositories/getUser/prisma-get-user";
 
 interface UpdateData {
   name?: string
-  password?: string
   stack?: string[]
 }
 
@@ -16,22 +13,13 @@ export const updateUser = async (req: Request, res: Response) => {
     return res.status(400).json({ error: 'Required user' })
   }
 
-  const { name, password, stack } = req.body
+  const { name, stack } = req.body
   const { id } = req.params
 
   const updateData: UpdateData = {}
 
-  if (!name && !password && !stack) {
-    return res.status(400).json({ error: 'At least one of name, password, or stack is required.' })
-  }
-
-  try {
-    if (password) {
-      passwordValidator.parse(password)
-      updateData.password = await bcrypt.hash(password, 10)
-    }
-  } catch (error: any) {
-    return res.status(400).json({ error: error.message })
+  if (!name && !stack) {
+    return res.status(400).json({ error: 'At least one of name or stack is required.' })
   }
 
   if (name !== undefined) {
