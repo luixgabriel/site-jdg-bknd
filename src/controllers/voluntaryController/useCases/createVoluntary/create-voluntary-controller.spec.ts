58af034180c031eba877099fcab03939f33ba5f7
Luis Gabriel <luisgasbriel@gmail.com>
2024-01-24 15:53:59 -0300

    first commit

---
+++ src/controllers/voluntaryController/useCases/createVoluntary/create-voluntary-controller.spec.ts
@@ -0,0 +1,5 @@
+describe('Create voluntary', () => {
+  it('should create a voluntary', () => {
+    expect(2 + 2).toBe(4)
+  })
+})
---
+++ tests/unit/utils/isWithinTimeLimit.ts
@@ -0,0 +1,39 @@
+// import { isWithinTimeLimit } from "@/utils/authUtils/isWithinTimeLimit";
+
+// describe('isWithinTimeLimit', () => {
+
+//   it('should return true when the time difference is within the time limit', () => {
+//     const currentDate = new Date(Date.now());
+
+//     const result = isWithinTimeLimit(currentDate, 5);
+
+//     expect(result).toBe(true);
+//   });
+
+//   it('should return false when the time difference exceeds the time limit', () => {
+//     const currentDate = new Date();
+//     const fiveMinutesAgo = new Date(currentDate.getTime() - (5 * 60 * 1000 + 1000));
+
+//     const result = isWithinTimeLimit(fiveMinutesAgo, 5);
+
+//     expect(result).toBe(false);
+//   });
+
+//   it('should return true when the time difference is exactly equal to the time limit', () => {
+//     const currentDate = new Date();
+//     const fiveMinutesAgo = new Date(currentDate.getTime() - (5 * 60 * 1000));
+
+//     const result = isWithinTimeLimit(fiveMinutesAgo, 5);
+
+//     expect(result).toBe(true);
+//   });
+
+//   it('should handle a zero time limit correctly', () => {
+//     const currentDate = new Date();
+//     const fiveMinutesAgo = new Date(currentDate.getTime() - (5 * 60 * 1000));
+
+//     const result = isWithinTimeLimit(fiveMinutesAgo, 0);
+
+//     expect(result).toBe(false); // Assuming zero time limit should always return false
+//   });
+// });
